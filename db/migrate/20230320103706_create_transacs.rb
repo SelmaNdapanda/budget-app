d508637988731d92f0ccd5f24a4e2f64b74615a0
Selma <selmandapanda@yahoo.com>
2023-03-20 12:52:29 +0200

    Create database Schema

--- db/migrate/20230320103706_create_transacs.rb
+++ db/migrate/20230320103706_create_transacs.rb
@@ -2,7 +2,9 @@ class CreateTransacs < ActiveRecord::Migration[7.0]
   def change
     create_table :transacs do |t|
       t.string :name
-      t.decimal :amout
+      t.decimal :amount
+      t.references :author, null: false, foreign_key: { to_table: :users}
+      t.references :group, null: false, foreign_key: true
 
       t.timestamps
     end
