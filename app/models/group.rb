fcada807e1143df1fad024e2e80f3731aecc71a0
Selma <selmandapanda@yahoo.com>
2023-03-20 14:56:01 +0200

    Add associations to models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,4 @@
 class Group < ApplicationRecord
+  belongs_to :user
+  has_many :transacts
 end
--- app/models/transac.rb
+++ app/models/transac.rb
@@ -1,2 +1,4 @@
 class Transac < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :group
 end
