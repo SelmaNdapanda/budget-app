a5c87ba1d050584b70c74bde4abb7a7cd7417a3e
Selma <selmandapanda@yahoo.com>
2023-03-21 11:20:38 +0200

    Create controller methods

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,30 @@
 class GroupsController < ApplicationController
+  load_and_authorize_resource
+  
+  def index
+    @groups = current_user.groups
+  end
+
+  def new
+    @new_group = Group.new
+  end
+      
+  def create
+    @new_group = Group.new(group_params)
+      
+    if @new_group.save
+      flash[:success] = 'Category created successfully.'
+      redirect_to groups_path
+    else
+      flash[:error] = 'Category could not be created.'
+      render 'new'
+    end
+  end
+      
+  private
+      
+  def group_params
+    params.require(:group).permit(:name, :icon, :user_id)
+  end     
 end
+
--- app/controllers/transacts_controller.rb
+++ app/controllers/transacts_controller.rb
@@ -1,2 +1,31 @@
 class TransactsController < ApplicationController
+  load_and_authorize_resource
+
+  def index
+    @group = Group.find(params[:group_id])
+    @group_transacs = Transac.where(group: @group).order(created_at: :desc)
+  end
+      
+  def new
+    @new_transac = Transac.new
+    @user_groups = Group.where(user: current_user)
+  end
+      
+  def create
+    @new_transac = Transac.new(transac_params)
+      
+    if @new_transac.save
+      flash[:success] = 'Transaction created successfully.'
+        redirect_to group_transacs_path(transac_params[:group_id])
+    else
+      flash[:error] = 'Transaction could not be created.'
+      render 'new'
+    end
+  end
+      
+  private
+      
+  def transac_params
+    params.require(:transac).permit(:name, :amount, :author_id, :group_id)
+  end
 end
