a48a2aa3fa0bec86c70f062f72781d546802bfe3
Selma <selmandapanda@yahoo.com>
2023-03-23 23:21:18 +0200

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,22 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:splash] unless Rails.env == 'test'
   
   protect_from_forgery with: :exception
   
-  before_action :update_allowed_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   
   protected
+
+  def after_sign_in_path_for(_resource)
+    groups_path
+  end
   
-  def update_allowed_parameters
+  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
       u.permit(:name, :email, :password, :password_confirmation)
     end
   end
-  
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, alert: exception.message
   end
