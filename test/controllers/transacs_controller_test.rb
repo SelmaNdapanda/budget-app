cbaebf63fc154f5ab90c48fd147b99a3869e10db
Selma <selmandapanda@yahoo.com>
2023-03-23 23:20:38 +0200

    Update transaction controller

--- test/controllers/transacs_controller_test.rb
+++ test/controllers/transacs_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class TransactsControllerTest < ActionDispatch::IntegrationTest
+class TransacsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
