af5cfecbf06b8c0667ba81224d3c3b088ae9cb22
Selma <selmandapanda@yahoo.com>
2023-03-23 23:22:06 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  root "splash#index"
+  
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :groups, only: [:index, :new, :create] do
+    resources :transacs, only: [:index]
+  end
+
+  resources :transacs, only: [:new, :create]
 end
